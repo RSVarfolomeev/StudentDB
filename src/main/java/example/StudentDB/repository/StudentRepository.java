dec530424bf0b86cfe55e8bec0db143097a55a53
VAR-VAR <hrom9090@mail.ru>
2022-06-07 23:25:37 +0300

    Fix StudentRepository

--- src/main/java/example/StudentDB/repository/StudentRepository.java
+++ src/main/java/example/StudentDB/repository/StudentRepository.java
@@ -4,10 +4,5 @@ package example.StudentDB.repository;
 import example.StudentDB.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    Student save(Student student);
-    List<Student> findAll();
-    void deleteById(long id);
 }
